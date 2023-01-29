32d204a50c21232cfd112fc78382b464c254e89f
Ada <adrianna.maria.zajac@gmail.com>
2023-01-29 12:34:54 +0100

    Added get courses request

--- src/app/all-courses/all-courses.component.ts
+++ src/app/all-courses/all-courses.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+class Course {
+  id: number = 0;
+  name: string = "";
+  description: string = "";
+}
 
 @Component({
   selector: 'app-all-courses',
   templateUrl: './all-courses.component.html',
   styleUrls: ['./all-courses.component.css']
 })
-export class AllCoursesComponent {
-  allCourses: string[] = ['Matematyka', "Fizyka", "Angielski", "Prolog"];
+export class AllCoursesComponent implements OnInit {
+  allCourses: Course[] = [];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<Course[]>("http://localhost:8080/courses")
+      .subscribe(courses => {
+        this.allCourses = courses;
+      });
+  }
 }
+
