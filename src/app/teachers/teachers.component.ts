bb5e1a07680847bcf36d2930fb884561ee7a7fff
Ada <adrianna.maria.zajac@gmail.com>
2023-03-14 14:12:16 +0100

    Edited displaying teachers

--- src/app/teachers/teachers.component.ts
+++ src/app/teachers/teachers.component.ts
@@ -11,7 +11,14 @@ export class TeachersComponent implements OnInit {
 
   teachers: Teacher[] = [
     {"name": "Andrzej", "surname": "Ziomek", "email": "and@zio.pl", "id":2},
-    {"name": "Andrzej1", "surname": "Ziomek1", "email": "and@zio.pl", "id":3}
+    {"name": "Andrzej", "surname": "Ziomek1", "email": "and@zio.pl", "id":3},
+    {"name": "Andrzej", "surname": "Ziomek2", "email": "and@zio.pl", "id":4},
+    {"name": "Andrzej", "surname": "Ziomek3", "email": "and@zio.pl", "id":5},
+    {"name": "Andrzej1", "surname": "Ziomek4", "email": "and@zio.pl", "id":6},
+    {"name": "Andrzej1", "surname": "Ziomek5", "email": "and@zio.pl", "id":7},
+    {"name": "Andrzej1", "surname": "Ziomek6", "email": "and@zio.pl", "id":8},
+    {"name": "Andrzej1", "surname": "Ziomek7", "email": "and@zio.pl", "id":9},
+    {"name": "Andrzej1", "surname": "Ziomek8", "email": "and@zio.pl", "id":10}
   ];
 
   displayedColumns: string[] = ['name', 'surname', 'email'];
@@ -20,7 +27,11 @@ export class TeachersComponent implements OnInit {
   }
 
   ngOnInit():void {
-    // this.httpClient.get<Teacher[]>("http://localhost:8080/teachers")
-    //   .subscribe(teachers => this.teachers = teachers);
+    this.httpClient.get<Teacher[]>("http://localhost:8080/teachers")
+      .subscribe(teachers => this.teachers = teachers);
+  }
+
+  test(){
+    console.log("aaaaaa")
   }
 }
