b0ef7f7a7c8efd5fe60d10e853f539ae3cdb8872
Ada <adrianna.maria.zajac@gmail.com>
2023-08-04 23:26:18 +0200

    Edited task-details component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {SessionService} from "../session.service";
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
-import {Material, Task} from "../api-models";
+import {Task} from "../api-models";
 
 @Component({
   selector: 'app-tasks',
@@ -11,15 +11,43 @@ import {Material, Task} from "../api-models";
 })
 export class TasksComponent {
   tasks: Task [] | undefined;
+  desired_columns = 3;
+  desired_row_height = '2:1';
 
   constructor(private route: ActivatedRoute, private httpClient: HttpClient, public sessionService: SessionService) {
   }
 
-  ngOnInit(){
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setAmountOfColumns(event.target.innerWidth);
+  }
+
+  ngOnInit() {
+    this.setAmountOfColumns(window.innerWidth);
     let id = this.route.snapshot.paramMap.get('id');
 
     this.httpClient.get<Task []>("http://localhost:8080/lessons/" + id + "/tasks").subscribe(tasks => this.tasks = tasks)
   }
 
+  getHeaderAmountOfColumnsInGrid() {
+    if (this.desired_columns < 3) {
+      return 1;
+    } else {
+      return 3;
+    }
+  }
 
+  getHeaderRowHeightInGrid() {
+    if (this.desired_columns == 2) {
+      return '8:1';
+    } else if (this.desired_columns == 1) {
+      return '5:1';
+    } else {
+      return '5:1';
+    }
+  }
+
+  private setAmountOfColumns(width: any) {
+    this.desired_columns = Math.trunc(width / 400);
+  }
 }
