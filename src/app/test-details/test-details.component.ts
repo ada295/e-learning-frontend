fbd5505e5938b5bd911c9acd05a7da81a7dffdee
Ada <adrianna.maria.zajac@gmail.com>
2023-09-15 22:36:22 +0200

    Edited test-details.component

--- src/app/test-details/test-details.component.ts
+++ src/app/test-details/test-details.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {Announcement, Lesson} from "../api-models";
 import {catchError, of} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-test-details',
   templateUrl: './test-details.component.html',
   styleUrls: ['./test-details.component.css']
 })
-export class TestDetailsComponent {
-  announcementGroup = this.formBuilder.group({
-    name: ['', Validators.required],
-    description: ['', Validators.required]
-  })
+export class TestDetailsComponent implements OnInit{
 
-  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) {
+  lessonId: string | null = '0';
+
+
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+   this.lessonId = this.route.snapshot.paramMap.get('id');
   }
 
 
-  }
+}
